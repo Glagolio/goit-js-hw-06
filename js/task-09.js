4bab4d6f966198f1ebe58f6209c886230a4d8f51
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-16 18:19:21 +0300

    Вирішив task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const colorBtnEl = document.querySelector('.change-color');
+const textOfColorEl = document.querySelector('.color');
+
+
+colorBtnEl.addEventListener('click', () => {
+  const colorOfBody = getRandomHexColor();
+  document.body.style.backgroundColor = colorOfBody;
+  textOfColorEl.textContent = colorOfBody;
+}
+)
