6165c5b84416d47417d21796bd2ed085f1e3806f
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-16 19:05:31 +0300

    Вирішив task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('#controls > input');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+
+function createBoxes() {
+  for (let i = 1; i <= inputEl.value; i += 1) {
+    const colorOfDiv = getRandomHexColor();
+    
+    const newDiv = document.createElement('div');
+    newDiv.style.width = `${30 + (i - 1) * 10}px`;
+    newDiv.style.height = `${30 + (i - 1) * 10}px`;
+    newDiv.style.backgroundColor = colorOfDiv;
+
+    manyDivs.push(newDiv);
+  }
+  boxesEl.append(...manyDivs);
+
+};
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  inputEl.value = null;
+}
+
+const manyDivs = [];
+
+btnCreateEl.addEventListener('click', createBoxes);
+btnDestroyEl.addEventListener('click', destroyBoxes)
+
