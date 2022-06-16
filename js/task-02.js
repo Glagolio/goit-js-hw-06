575ca64053958ed95f23d0e7cbcea04ecf1769fc
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-16 13:49:45 +0300

    Зробив 2 завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredientsEl = document.querySelector('#ingredients');
+
+const listArray = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("item");
+  return ingredientEl
+});
+
+
+listOfIngredientsEl.append(...listArray);
+
+
+
+
